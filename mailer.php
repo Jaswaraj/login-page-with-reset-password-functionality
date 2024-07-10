eab4f9ad06093215f351d622ced5c3c43d5ccbc0
Jaswaraj Sahoo <112747653+Jaswaraj@users.noreply.github.com>
2024-07-10 23:42:13 +0530

    mailer.php

--- mailer.php
+++ mailer.php
@@ -8,16 +8,14 @@ require __DIR__ . "/vendor/autoload.php";
 
 $mail = new PHPMailer(true);
 
- $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 
-$mail->Host = "smtp.example.com";
+$mail->Host = "smtp.gmail.com";
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
-$mail->Username = "your-user@example.com";
-$mail->Password = "your-password";
+$mail->Username = "jaswarajsahoo07@gmail.com";
+$mail->Password = "izlwnkmqcmdjdboc";
 
 $mail->isHtml(true);
 
